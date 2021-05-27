b4c20dae69ac5be912bd9802d334db8add1d4fb0
Leo <leoisbeste@qq.com>
2021-05-28 00:48:18 +0800

    feat: lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -2,7 +2,7 @@ package lexer
 
 import (
 	"bd/lexer/util"
-	// "path/filepath"
+	"path/filepath"
 	"os"
 	"io"
 	"bytes"
@@ -17,7 +17,7 @@ type Lexer struct {
 }
 
 // 实例化一个语法分析器
-func NewLexer(r *io.Reader, endToken string) *Lexer {
+func NewLexer(r io.Reader, endToken string) *Lexer {
 	s := util.NewStream(r, EndToken)
 	return &Lexer {
 		Stream: s,
@@ -28,7 +28,7 @@ func NewLexer(r *io.Reader, endToken string) *Lexer {
 
 // 文件传入数据
 func FileLexer(relatePath string) *Lexer {
-	absPath, err := filepath(relatePath)
+	absPath, err := filepath.Abs(relatePath)
 	if err != nil {
 		panic(err)
 	}
@@ -43,7 +43,7 @@ func FileLexer(relatePath string) *Lexer {
 
 // 从字面量数据传入(test)
 func Analyse(source string) []*Token  {
-	return NewLexer(bytes.NewBufferString(source), endToken)
+	return NewLexer(bytes.NewBufferString(source), EndToken).Analyse()
 }
 
 // Lexer函数方法
@@ -113,14 +113,43 @@ func (this *Lexer) Analyse() []*Token {
 		}
 
 		if IsLetter(c) {
-			l.PutBack(c)
-			tokens = append(tokens, l.MakeVarOrKeyword())
+			// 判定是否为正确的单词，放回，由后续函数接手
+			this.PutBack(c)
+			tokens = append(tokens, this.MakeVarOrKeyword())
 			continue
 		}
+		// 数字字面量处理
+		if IsNumber(c) {
+			this.PutBack(c)
+			tokens = append(tokens, this.MakeNumber())
+		}
 
+		// 优先判定表达式，若非表达式，再定义为单独符号
+		// 3+5 .5 3 * 5
+		if (c == "+" || c == "-" || c == ".") && IsNumber(lookahead) {
+			var lastToken *Token = nil
+			if len(tokens) > 0 {
+				lastToken = tokens[len(tokens)-1]
+			}
+
+			if nil == lastToken || !lastToken.IsValue() || lastToken.IsOperator() {
+				this.PutBack(c)
+				tokens = append(tokens, this.MakeNumber())
+				continue
+			}
+		}
+
+		if IsOperator(c) {
+			this.PutBack(c)
+			tokens = append(tokens, this.MakeOp())
+			continue
+		}
+		panic("unexpected character" + c)
 	}
+	return tokens
 }
 
+// 字符串处理函数
 func (this *Lexer) MakeString() *Token{
 	s := ""
 	state := 0
@@ -151,6 +180,156 @@ func (this *Lexer) MakeString() *Token{
 	panic("String syntax error")
 }
 
-func IsLetter()  {
-	
+// 单词处理函数（可能为关键字或变量）
+func (this *Lexer) MakeVarOrKeyword() *Token {
+	s := ""
+	for this.HasNext() {
+		lookahead := this.Peek()
+		if IsLiteral(lookahead) {
+			s += lookahead
+			// this.Next()
+		} else {
+			break
+		}
+		this.Next()
+	}
+
+	if(IsKeyword(s)) {
+		return NewToken(KEYWORD, s)
+	}
+
+	if "true" == s || "false" == s {
+		return NewToken(BOOLEAN, s)
+	}
+
+	return NewToken(VARIABLE, s)
 }
+
+// 数字字面量处理函数(此处兼容了字面量表达式的处理)
+func (this *Lexer) MakeNumber() *Token {
+	state := 0
+	s := ""
+	for this.HasNext() {
+		lookahead := this.Peek()
+		switch state {
+		// 初始化case
+		case 0:
+			if "0" == lookahead {
+				state = 1
+			} else if IsNumber(lookahead) {
+				state = 2
+			} else if "+" == lookahead || "-" == lookahead {
+				state = 3
+			} else if "." == lookahead {
+				state = 5
+			}
+		// 如果数字以0开头，必须要跳过所有0
+		case 1:
+			if "0" == lookahead {
+				state = 1
+			} else if IsNumber(lookahead) {
+				state = 2
+			} else if lookahead == "." {
+				state = 4
+			} else {
+				return NewToken(INTEGER, s)
+			}
+		// 如果是数字
+		case 2:
+			if IsNumber(lookahead) {
+				state = 2
+			} else if lookahead == "." {
+				state = 4
+			} else {
+				return NewToken(INTEGER, s)
+			}
+		// 
+		case 3:
+			if IsNumber(lookahead) {
+				state = 2
+			} else if lookahead == "." {
+				state = 5
+			} else {
+				panic("unexpected number " + lookahead)
+			}
+		
+		case 4:
+			if lookahead == "." {
+				panic("unexpected number " + lookahead)
+			} else if IsNumber(lookahead) {
+				state = 20
+			} else {
+				return NewToken(FLOAT, s)
+			}
+		case 5:
+			if IsNumber(lookahead) {
+				state = 20
+			} else {
+				panic("unexpected number " + lookahead)
+			}
+
+		case 20:
+			if IsNumber(lookahead) {
+				state = 20
+			} else if "." == lookahead {
+				panic("unexpected number " + lookahead)
+			} else {
+				return NewToken(FLOAT, s)
+			}
+			
+		}
+
+		s += lookahead
+		this.Next()
+	}
+
+	panic("make number fail")
+}
+
+func (this *Lexer) MakeOp() *Token {
+	state := 0
+
+	for this.HasNext() {
+		// 这里不需要peek预检，因为不存在二义性，直接消耗字符即可
+		lookahead := this.Next()
+		switch state {
+		case 0:
+			switch lookahead {
+			case "+":
+				state = 1
+			case "-":
+				state = 2
+			case "*":
+				state = 3
+			case `/`:
+				state = 4
+			case `>`:
+				state = 5
+			case `<`:
+				state = 6
+			case `=`:
+				state = 7
+			case `!`:
+				state = 8
+			case `&`:
+				state = 9
+			case `|`:
+				state = 10
+			case `^`:
+				state = 11
+			case `%`:
+				state = 12
+			case ",":
+				return NewToken(OPERATOR, ",")
+			case ";":
+				return NewToken(OPERATOR, ";")
+			}
+		case 1:
+			switch lookahead {
+			case condition:
+				
+			}
+		}
+	}
+} 
+
